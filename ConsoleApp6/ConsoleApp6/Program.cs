f95b66c4c0bc7f132c14327d21dcc5560b34745f
eminnirahimov <133598771+eminnirahimov@users.noreply.github.com>
2023-05-24 00:16:14 +0300

    Revert "z2"
    This reverts commit dbd4e46ba2bc97472c6a561dfa87a26e533324f8.

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "bella roller label";
+            Regex reg = new Regex(@"\w+(-\w+)*");
+
+            var words = reg.Matches(txt).Cast<Match>().Select(i => i.Value).ToArray();
+            foreach (var w in words)
+            {
+                Console.WriteLine(w);
+                HashSet<char> hs = new HashSet<char>(w.ToLower());
+                Console.WriteLine(string.Join(" ", hs));
+                Console.WriteLine(new string('*', 6));
+            }
+            Console.ReadKey();
+        }
+    }
+}
